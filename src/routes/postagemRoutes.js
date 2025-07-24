1a07f45a0978908e02d59c03750506d3d75cdd68
Tiago <t.mendescarvalho123@gmail.com>
2025-07-24 02:44:15 -0300

    docker-compose :muscle:

--- src/routes/postagemRoutes.js
+++ src/routes/postagemRoutes.js
@@ -3,4 +3,9 @@ import PostagemController from "../controllers/postagemController.js";
 
 const routes = express.Router();
 
-routes.get("postagens", PostagemController.listarPostagens);
+routes.get("/postagens", PostagemController.listarPostagens);
+routes.post("/postagens", PostagemController.cadastrarPostagem);
+routes.put("/postagens/:id", PostagemController.AtualizarLivro);
+routes.delete("/postagens/:id", PostagemController.removePostagem);
+
+export default routes;
